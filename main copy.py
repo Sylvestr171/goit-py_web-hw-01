9388456c30fb52ea4f040c9ea34ab5536226f153
Oleksii Haiduk <Silvestr171@gmail.com>
2025-06-10 15:42:38 +0300

    реалізація інтерфейс класу

--- main copy.py
+++ main copy.py
@@ -34,7 +34,7 @@ class UserInterface(ABC):
     @abstractmethod
     def birthdays(self, book: AddressBook) -> str:
         pass
-    
+
     @abstractmethod
     def show_help(self) -> str:
         pass
@@ -59,47 +59,47 @@ def parse_input(user_input:str) -> tuple[str,*tuple[str,...]]:
     cmd = cmd.strip().lower()
     return cmd, *args
 
-#Функція лолавання контакту Команда: "add John 1234567890"
-@input_error
-def add_contact(args:list[str], contacts:AddressBook) -> str:
-    try:
-        name, phone, *_ = args
-    except ValueError:
-        return f"Give me correct name and phone please."
-    name=name.lower().capitalize()
-    record = contacts.find(name)
-    message = "Contact updated."
-    if record is None:
-        record = Record(name)
-        contacts.add_record(record)
-        message = "Contact added."
-    if phone:
-        record.add_phone(phone)
-    return message
+# #Функція лолавання контакту Команда: "add John 1234567890"
+# @input_error
+# def add_contact(args:list[str], contacts:AddressBook) -> str:
+#     try:
+#         name, phone, *_ = args
+#     except ValueError:
+#         return f"Give me correct name and phone please."
+#     name=name.lower().capitalize()
+#     record = contacts.find(name)
+#     message = "Contact updated."
+#     if record is None:
+#         record = Record(name)
+#         contacts.add_record(record)
+#         message = "Contact added."
+#     if phone:
+#         record.add_phone(phone)
+#     return message
 
-#Функція зміни контакту  Команда: "change John 0987654321"
-@input_error
-def change_contact(args:list[str], book:AddressBook) -> str:
-    try:
-        name, old_phone, new_phone = args
-    except ValueError:
-        return f'"Uncnown command\nchange <name> <old_phone_namer> <new_phone_namer>" - change the phone number in the address book'
-    if name.lower().capitalize() not in book.keys():
-        massage = 'Contact is missing, please add it (add <name> <phone_namer>)! '
-    else: 
-        book[name.lower().capitalize()].edit_phone(old_phone, new_phone)
-        massage = 'Contact updated.'
-    return massage
+# #Функція зміни контакту  Команда: "change John 0987654321"
+# @input_error
+# def change_contact(args:list[str], book:AddressBook) -> str:
+#     try:
+#         name, old_phone, new_phone = args
+#     except ValueError:
+#         return f'"Uncnown command\nchange <name> <old_phone_namer> <new_phone_namer>" - change the phone number in the address book'
+#     if name.lower().capitalize() not in book.keys():
+#         massage = 'Contact is missing, please add it (add <name> <phone_namer>)! '
+#     else: 
+#         book[name.lower().capitalize()].edit_phone(old_phone, new_phone)
+#         massage = 'Contact updated.'
+#     return massage
 
-#Функція показати контакт Команда: "phone John"
-def show_phone(args:list[str], contacts:AddressBook) -> str:
-    name = args[0].lower().capitalize()
-    return contacts.get(name, 'The name is missing')
+# #Функція показати контакт Команда: "phone John"
+# def show_phone(args:list[str], contacts:AddressBook) -> str:
+#     name = args[0].lower().capitalize()
+#     return contacts.get(name, 'The name is missing')
 
-#Функція виведення всієї адресної книги Команда: "all"
-def show_all(book:AddressBook) -> str:
+# #Функція виведення всієї адресної книги Команда: "all"
+# def show_all(book:AddressBook) -> str:
 
-    return '\n'.join(f"{key} => {value}" for key, value in book.items())
+#     return '\n'.join(f"{key} => {value}" for key, value in book.items())
 
 #функція для вибору рандомної фрази для відповіді на hello
 @input_error
@@ -109,65 +109,164 @@ def get_random_phrase() -> str:
             phrase = file.readlines()
             return choice(phrase).strip()
 
-#функція для додавання дати народження. Команда add-birthday <name> <DD.MM.YYYY>
-@input_error
-def add_birthday(args: list[str], book: AddressBook) -> str:
-    try:
-        name, birthday, *_ = args
-    except ValueError:
-        return f"Give me name and birthday."
-    name=name.lower().capitalize()
-    record = book.find(name)
-    message = "Contact updated."
-    if record is None:
-        record = Record(name)
-        book.add_record(record)
-        message = "Contact added."
-    if birthday:
-        record.add_birthday(birthday)
-    return message
+# #функція для додавання дати народження. Команда add-birthday <name> <DD.MM.YYYY>
+# @input_error
+# def add_birthday(args: list[str], book: AddressBook) -> str:
+#     try:
+#         name, birthday, *_ = args
+#     except ValueError:
+#         return f"Give me name and birthday."
+#     name=name.lower().capitalize()
+#     record = book.find(name)
+#     message = "Contact updated."
+#     if record is None:
+#         record = Record(name)
+#         book.add_record(record)
+#         message = "Contact added."
+#     if birthday:
+#         record.add_birthday(birthday)
+#     return message
 
-#функція для виводу дати народження для вказаного контакту. Команда show-birthday <name>
-@input_error
-def show_birthday(args: list[str], book: AddressBook) -> Birthday:
-    name = args[0].lower().capitalize()
-    result=book.get(name, None)
-    if result != None:
-        if result.birthday != None:
-            return result.birthday
-        else:
-            raise CastomError("Дата відсутня")
-    else:
-        raise CastomError("Контакт відсутній")
+# #функція для виводу дати народження для вказаного контакту. Команда show-birthday <name>
+# @input_error
+# def show_birthday(args: list[str], book: AddressBook) -> Birthday:
+#     name = args[0].lower().capitalize()
+#     result=book.get(name, None)
+#     if result != None:
+#         if result.birthday != None:
+#             return result.birthday
+#         else:
+#             raise CastomError("Дата відсутня")
+#     else:
+#         raise CastomError("Контакт відсутній")
 
-#функціядля виводу дати привітання працівників на найближчий тиждень. Команда birthdays
-@input_error
-def birthdays(book: AddressBook) -> str:
-    results=book.get_upcoming_birthdays()
-    if results:
-        return str(results)
-    else: 
-        return f'No birthdays for display'
+# #функціядля виводу дати привітання працівників на найближчий тиждень. Команда birthdays
+# @input_error
+# def birthdays(book: AddressBook) -> str:
+#     results=book.get_upcoming_birthdays()
+#     if results:
+#         return str(results)
+#     else: 
+#         return f'No birthdays for display'
     
-def show_help() -> str:
-    help_message="""The bot helps to work with the contact book.
-                            Commands and functions:
-                            "close" | "exit" - exit the program
-                            "hello" - display a greeting
-                            "add <name> <phone_namer>" - add a phone number to the address book
-                            "change <name> <old_phone_namer> <new_phone_namer>" - change the phone number in the address book
-                            "add-birthday <name> <DD.MM.YYYY>" - add a birthday to the address book
-                            "show-birthday <name>" - show birthday
-                            "phone <name>" - show the number
-                            "all" - show the entire address book
-                            "birthdays" - show date for congratulation
-                            "help" | "?" - show this help"""
-    return help_message
+# def show_help() -> str:
+#     help_message="""The bot helps to work with the contact book.
+#                             Commands and functions:
+#                             "close" | "exit" - exit the program
+#                             "hello" - display a greeting
+#                             "add <name> <phone_namer>" - add a phone number to the address book
+#                             "change <name> <old_phone_namer> <new_phone_namer>" - change the phone number in the address book
+#                             "add-birthday <name> <DD.MM.YYYY>" - add a birthday to the address book
+#                             "show-birthday <name>" - show birthday
+#                             "phone <name>" - show the number
+#                             "all" - show the entire address book
+#                             "birthdays" - show date for congratulation
+#                             "help" | "?" - show this help"""
+#     return help_message
+
+class ConsoleInterface(UserInterface):
+    #Функція лолавання контакту Команда: "add John 1234567890"
+    @input_error
+    def add_contact(self, args:list[str], contacts:AddressBook) -> str:
+        try:
+            name, phone, *_ = args
+        except ValueError:
+            return f"Give me correct name and phone please."
+        name=name.lower().capitalize()
+        record = contacts.find(name)
+        message = "Contact updated."
+        if record is None:
+            record = Record(name)
+            contacts.add_record(record)
+            message = "Contact added."
+        if phone:
+            record.add_phone(phone)
+        return message
+
+    #Функція зміни контакту  Команда: "change John 0987654321"
+    @input_error
+    def change_contact(self, args:list[str], book:AddressBook) -> str:
+        try:
+            name, old_phone, new_phone = args
+        except ValueError:
+            return f'"Uncnown command\nchange <name> <old_phone_namer> <new_phone_namer>" - change the phone number in the address book'
+        if name.lower().capitalize() not in book.keys():
+            massage = 'Contact is missing, please add it (add <name> <phone_namer>)! '
+        else: 
+            book[name.lower().capitalize()].edit_phone(old_phone, new_phone)
+            massage = 'Contact updated.'
+        return massage
+    
+    #Функція показати контакт Команда: "phone John"
+    def show_phone(self, args:list[str], contacts:AddressBook) -> str:
+        name = args[0].lower().capitalize()
+        return contacts.get(name, 'The name is missing')
+    
+    #Функція виведення всієї адресної книги Команда: "all"
+    def show_all(self, book:AddressBook) -> str:
+
+        return '\n'.join(f"{key} => {value}" for key, value in book.items())
+    
+    #функція для додавання дати народження. Команда add-birthday <name> <DD.MM.YYYY>
+    @input_error
+    def add_birthday(self, args: list[str], book: AddressBook) -> str:
+        try:
+            name, birthday, *_ = args
+        except ValueError:
+            return f"Give me name and birthday."
+        name=name.lower().capitalize()
+        record = book.find(name)
+        message = "Contact updated."
+        if record is None:
+            record = Record(name)
+            book.add_record(record)
+            message = "Contact added."
+        if birthday:
+            record.add_birthday(birthday)
+        return message
+    
+    #функція для виводу дати народження для вказаного контакту. Команда show-birthday <name>
+    @input_error
+    def show_birthday(self, args: list[str], book: AddressBook) -> Birthday:
+        name = args[0].lower().capitalize()
+        result=book.get(name, None)
+        if result != None:
+            if result.birthday != None:
+                return result.birthday
+            else:
+                raise CastomError("Дата відсутня")
+        else:
+            raise CastomError("Контакт відсутній")
+
+    #функціядля виводу дати привітання працівників на найближчий тиждень. Команда birthdays
+    @input_error
+    def birthdays(self, book: AddressBook) -> str:
+        results=book.get_upcoming_birthdays()
+        if results:
+            return str(results)
+        else: 
+            return f'No birthdays for display'
+
+    def show_help(self) -> str:
+        help_message="""The bot helps to work with the contact book.
+                                Commands and functions:
+                                "close" | "exit" - exit the program
+                                "hello" - display a greeting
+                                "add <name> <phone_namer>" - add a phone number to the address book
+                                "change <name> <old_phone_namer> <new_phone_namer>" - change the phone number in the address book
+                                "add-birthday <name> <DD.MM.YYYY>" - add a birthday to the address book
+                                "show-birthday <name>" - show birthday
+                                "phone <name>" - show the number
+                                "all" - show the entire address book
+                                "birthdays" - show date for congratulation
+                                "help" | "?" - show this help"""
+        return help_message
 
 def main() -> Union[str, None]:
     # book = AddressBook()
     saver = SaveData()
     book = saver.load_data()
+    console_interface = ConsoleInterface()
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
@@ -181,21 +280,21 @@ def main() -> Union[str, None]:
                 case "hello":
                     print(get_random_phrase())
                 case "add":
-                    print(add_contact(args, book))
+                    print(console_interface.add_contact(args, book))
                 case "change":
-                    print(change_contact(args, book))
+                    print(console_interface.change_contact(args, book))
                 case "phone":
-                    print(show_phone(args, book))
+                    print(console_interface.show_phone(args, book))
                 case "all":
-                    print(show_all(book))
+                    print(console_interface.show_all(book))
                 case "add-birthday":
-                    print(add_birthday(args, book))
+                    print(console_interface.add_birthday(args, book))
                 case "show-birthday":
-                    print(show_birthday(args, book))
+                    print(console_interface.show_birthday(args, book))
                 case "birthdays":
-                    print(birthdays(book))
+                    print(console_interface.birthdays(book))
                 case "help" | "?":
-                    print(show_help())             
+                    print(console_interface.show_help())             
                 case _:
                     print("Invalid command.\nFor help enter: ?, help")
         except TypeError:
